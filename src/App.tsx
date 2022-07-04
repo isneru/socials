561008366bdeb9aadf3e1139fc5444c99d8ec802
isneru <diogoclanogueira@gmail.com>
2022-07-04 21:41:18 +0100

    Implemented Framer-Motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 import { Link } from "./components";
 import {
@@ -10,21 +11,64 @@ import {
 } from "phosphor-react";
 
 export const App: React.FC = () => {
+  const list = {
+    visible: {
+      opacity: 1,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.1,
+      },
+    },
+    hidden: { opacity: 0 },
+  };
+
+  const profile = {
+    visible: {
+      opacity: 1,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.1,
+      },
+    },
+    hidden: { opacity: 0 },
+  };
+
+  const item = {
+    visible: { opacity: 1, x: 0 },
+    hidden: { opacity: 0, x: 100 },
+  };
   return (
     <div className="w-screen h-screen flex justify-center">
       <div className="flex flex-col justify-center items-center gap-8 w-2/4">
-        <div className="flex flex-col justify-center items-center">
-          <img
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={profile}
+          className="flex flex-col justify-center items-center">
+          <motion.img
+            variants={item}
+            drag
+            whileTap={{ scale: 1.05, zIndex: 99 }}
+            dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
+            dragElastic={1}
             src="https://avatars.githubusercontent.com/u/89457639?v=4"
             alt="Profile Picture"
-            className="rounded-full border-4 border-buttonstroke max-h-[14vh] mb-2"
+            className="rounded-full border-4 border-buttonstroke max-h-[14vh] mb-2 cursor-grab"
           />
-          <h1 className="font-semibold text-headline">Diogo Nogueira</h1>
-          <span className="leading-none text-paragraph font-medium">
+          <motion.h1 variants={item} className="font-semibold text-headline">
+            Diogo Nogueira
+          </motion.h1>
+          <motion.span
+            variants={item}
+            className="leading-none text-paragraph font-medium">
             @isneru
-          </span>
-        </div>
-        <ul className="flex flex-col justify-center items-center gap-5 w-full">
+          </motion.span>
+        </motion.div>
+        <motion.ul
+          initial="hidden"
+          animate="visible"
+          variants={list}
+          className="flex flex-col justify-center items-center gap-5 w-full">
           <Link
             href="https://github.com/isneru"
             text="Github"
@@ -80,15 +124,17 @@ export const App: React.FC = () => {
               />
             }
           />
-        </ul>
+        </motion.ul>
       </div>
       <footer className="w-full h-16 flex justify-center items-center fixed bottom-0 bg-background">
-        <span className="text-highlight font-light">
+        <motion.span
+          animate={{ y: [100, 0], opacity: [0, 1] }}
+          className="text-highlight font-light">
           Made with 💜 by{" "}
           <a href="https://github.com/isneru" target="_blank">
             Diogo Nogueira
           </a>
-        </span>
+        </motion.span>
       </footer>
     </div>
   );
--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 interface LinkHelperProps {
   href: string;
@@ -7,8 +8,14 @@ interface LinkHelperProps {
 }
 
 export const Link: React.FC<LinkHelperProps> = ({ children, href, text }) => {
+  const item = {
+    visible: { opacity: 1, x: 0 },
+    hidden: { opacity: 0, x: -100 },
+  };
   return (
-    <li className="bg-button hover:bg-highlight text-buttontext py-5 w-full sm:w-3/4 md:w-1/2 xl:1/3 2xl:w-1/3 wide:w-1/4 rounded-xl shadow-xl flex justify-center items-center transition-colors">
+    <motion.li
+      variants={item}
+      className="bg-button hover:bg-highlight text-buttontext py-5 w-full sm:w-3/4 md:w-1/2 xl:1/3 2xl:w-1/3 wide:w-1/4 rounded-xl shadow-xl flex justify-center items-center transition-colors">
       <a
         href={href}
         target="_blank"
@@ -16,6 +23,6 @@ export const Link: React.FC<LinkHelperProps> = ({ children, href, text }) => {
         {children}
         <span className="hidden xs:inline">{text}</span>
       </a>
-    </li>
+    </motion.li>
   );
 };
