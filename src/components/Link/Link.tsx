06855c86afd0796f5917df7beb899f8222cfb86d
isneru <diogoclanogueira@gmail.com>
2022-07-05 10:37:35 +0100

    Fixed Li Spacing

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -15,11 +15,11 @@ export const Link: React.FC<LinkHelperProps> = ({ children, href, text }) => {
   return (
     <motion.li
       variants={item}
-      className="bg-button hover:bg-highlight text-buttontext py-5 w-full sm:w-3/4 md:w-1/2 xl:1/3 2xl:w-1/3 wide:w-1/4 rounded-xl shadow-xl flex justify-center items-center transition-colors">
+      className="bg-button hover:bg-highlight text-buttontext  w-full sm:w-3/4 md:w-1/2 xl:1/3 2xl:w-1/3 wide:w-1/4 rounded-xl shadow-xl flex justify-center items-center transition-colors">
       <a
         href={href}
         target="_blank"
-        className="w-full flex justify-center items-center font-medium relative ">
+        className="py-5 w-full flex justify-center items-center font-medium relative ">
         {children}
         <span className="hidden xs:inline">{text}</span>
       </a>
